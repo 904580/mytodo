8a4ce8519a0b45509ac994c7d34eea1300062374
anoop saini <anoopsaini421@gmail.com>
2021-11-24 21:45:50 -0800

    todoapp

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import TodoList from '../components/TodoList/TodoList';
+
+const Home = () => {
+    console.log('Home Component =======>>');
+    return (
+    <>
+        <h3>Todo App </h3>
+        <TodoList />
+    </>
+)};
+
+export default Home;
